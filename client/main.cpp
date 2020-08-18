fa5613b648a36309e726eced396ebfc9cfbba914
leon <yigeren010@gmail.com>
2020-08-18 22:32:37 +0900

    .....

--- client/main.cpp
+++ client/main.cpp
@@ -8,8 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
-    QByteArray b = " 230";
-    qDebug()<< "b:"<<b.toInt() + 1;
     w.show();
     return a.exec();
 }
--- client/mylabel.cpp
+++ client/mylabel.cpp
@@ -9,16 +9,16 @@ myLabel::myLabel(QWidget *parent) : QLabel(parent)
 void myLabel::paintEvent(QPaintEvent *event){
     QLabel::paintEvent(event);
 
-    QPoint startPoint(100,100);
-    QPoint endPoint(500,500);
+    QPoint startPoint(0,0);
+    QPoint endPoint(0,0);
     QColor lineColor(Qt::red);
-    int lineSize = 2;
+//    int lineSize = 2;
 
 
     QPainter painter(this);
     QPen pen;
     pen.setColor(lineColor);
-    pen.setWidth(lineSize);
+//    pen.setWidth(lineSize);
     painter.setPen(pen);
     painter.drawRect(QRect(startPoint.x(), startPoint.y(),
                            endPoint.x()-startPoint.x(), endPoint.y()-startPoint.y()));
